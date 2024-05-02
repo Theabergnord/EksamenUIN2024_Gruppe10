05fc1b91e745a723834350416ab3f16dc9cd51f9
emilycostas <emilyenger@msn.com>
2024-05-02 14:22:19 +0200

    Lagt til ikoner
    Lagt til ikoner i nav + småendringer i SASS/CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import {Link} from "react-router-dom"
+import { TbMovie } from "react-icons/tb"
+import { FaUserCircle } from "react-icons/fa";
 
 export default function Header(){
     
@@ -7,9 +9,9 @@ export default function Header(){
         <header>
             <h1><Link to='/'>What to see?</Link></h1>
             <nav>
-                <Link to='/' className="Hjem_side">Hva skal jeg se?</Link>
+                <Link to='/' className="Hjem_side"> <TbMovie className="filmIkon" /> Hva skal jeg se?</Link>
                 <Link to='genre'>Bla gjennom sjangere</Link>
-                <Link to='#'>Bruker?</Link>
+                <Link to='#'> <FaUserCircle className="brukerIkon" /> Bruker? </Link>
             </nav>
         </header>
         
