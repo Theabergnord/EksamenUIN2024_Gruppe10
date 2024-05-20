f08a7f44070f177779c30d3d363f9df2ae87efc9
emilycostas <emilyenger@msn.com>
2024-05-20 17:45:11 +0200

    Kommentarer

--- frontend/src/components/UserContext.jsx
+++ frontend/src/components/UserContext.jsx
@@ -6,6 +6,8 @@ const UserContext = createContext(null)
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null)
 
+    /*En bruker blir lagret i localStorage når man logger den inn. Må slette bruker id'en i
+    Application for å få opp alle tre etter første innlogging, og deretter refreshe.*/
     useEffect(() => {
         const loadCurrentUser = async () => {
             const savedUserId = localStorage.getItem('selectedUserId')
@@ -17,6 +19,7 @@ export const UserProvider = ({ children }) => {
         loadCurrentUser()
     }, [])
 
+    //Bruker Groq-spørring for å hente id, navn, ønskeliste og favorittliste.
     const fetchUserData = async (userId) => {
         const query = `*[_type == "user" && _id == $userId]{ _id, name, wishlist, favorites }`
         const params = { userId: userId }
