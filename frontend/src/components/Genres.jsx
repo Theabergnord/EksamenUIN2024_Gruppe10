59bbcae196f3f0c360327b38525c150b2753e853
emilycostas <emilyenger@msn.com>
2024-05-20 17:18:36 +0200

    Endret småting

--- frontend/src/components/Genres.jsx
+++ frontend/src/components/Genres.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { client } from '../../sanity/client'
 import { Link } from 'react-router-dom'
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 export default function Genres(){
   const [genre, setGenre] = useState([])
+  const [favoriteGenres, setFavoritesGenres] = useState([])
 
   useEffect(() => {
     const getGenre = async () => {
@@ -18,6 +21,10 @@ export default function Genres(){
     getGenre()
   }, [])
 
+  const handleFavorite = (genre) => {
+    setFavoritesGenres((prevFavorites) => [...prevFavorites, genre])
+  }
+
 
   return(
     <div className='genre'>
@@ -26,7 +33,15 @@ export default function Genres(){
         {genre.map((g) => (
           <li key={g.genre}>
             <Link to={`/genres/${g.genre}`}>{g.genre}</Link>
-            <button className='favoriteGenre'>Legg til som favoritt</button>
+
+          {/*Kilde: Hvordan sjekke om knappen er klikket eller ikke https://codedamn.com/news/reactjs/if-else-statements-in-jsx*/}
+            <button className='favoriteGenre' onClick={() => handleFavorite(g.genre)}>
+              {favoriteGenres.includes(g.genre) ? (
+                <> Favorittsjanger <FaStar className='stjerne'/> </>
+              ) : (
+                <> Legg til som favoritt <FaRegStar className='stjerne'/> </>
+              )}
+            </button>
           </li>
         ))}
       </ul>
