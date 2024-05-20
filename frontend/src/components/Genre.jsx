f6b2aee2ce49cc11bee846ccb73fff59f8552764
emilycostas <emilyenger@msn.com>
2024-05-20 14:13:35 +0200

    Lagt inn endringer for Thea i oppdatert branch.
    Thea har laget teller for å telle antall filmer i hver sjanger, og endret slik at filmer blir hentet opp i riktig sjanger på sjangerside.

--- frontend/src/components/Genre.jsx
+++ frontend/src/components/Genre.jsx
@@ -1,8 +1,41 @@
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { client } from '../../sanity/client'
+import MovieCard from './MovieCard'
 
-export default function Genre(){
-    const { genre } = useParams();
-    return(
-        <h2>{genre}</h2>
-    )
+export default function Genre() {
+  const { genre } = useParams()
+  const [films, setFilms] = useState([])
+  const [movieCount, setMovieCount] = useState(0)
+
+  useEffect(() => {
+    const filmGenre = async () => {
+      try {
+        const data = await client.fetch(
+          `*[_type == "films" && genre == $genre]{
+            title,
+            genre,
+            imdbid
+          }`,
+          { genre }
+        );
+
+        setMovieCount(data.length)
+
+        const imdbIds = data.map(film => film.imdbid)
+        setFilms(imdbIds);
+      } catch (error) {
+        console.error('Klarte ikke å hente filmer', error)
+      }
+    }
+
+    filmGenre()
+  }, [genre])
+
+  return (
+    <div>
+      <h2>Filmer i sjangeren: {genre} ({movieCount})</h2>
+      <MovieCard movies={films} type="genre" />
+    </div>
+  )
 }
